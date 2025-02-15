b5b16912b9497ef8afa59744a86c07fe21c89618
jhonpabz <jhncsrpbl@gmail.com>
2025-02-15 13:48:35 +0800

    mod: seeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -11,9 +11,9 @@ class AdminUserSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => '333',
-            'email' => env('ADMIN_EMAIL', '333@jhonpabz.com'),
-            'password' => Hash::make(env('ADMIN_PASSWORD', '333')),
+            'name' => 'Jhonpabz',
+            'email' => env('ADMIN_EMAIL'),
+            'password' => Hash::make(env('ADMIN_PASSWORD')),
         ]);
     }
 }
